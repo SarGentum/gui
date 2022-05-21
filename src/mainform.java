806966b4318d79fcf4b95d94738a7868086af61b
Sragentum <stamgand@yandex.ru>
2022-05-22 03:22:55 +0400

    First GIU

---
+++ src/mainform.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class mainform {
+    private JButton button1;
+    private JPanel panel1;
+    private JRadioButton radioButton1;
+    private JCheckBox checkBox1;
+    private JComboBox comboBox1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
